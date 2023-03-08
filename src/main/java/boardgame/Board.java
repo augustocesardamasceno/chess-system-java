56257d65264e02dfa754768157b57a85780d4809
augustocesardamasceno <augustoccmd@hotmail.com>
2023-03-08 15:34:05 -0300

    Moving pieces

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import java.awt.*;
+
 public class Board {
     private int rows;
     private int columns;
@@ -41,6 +43,19 @@ public class Board {
         }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public Piece removePiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
     }
 
     private boolean positionExists(int row, int column){
